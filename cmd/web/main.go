6e126229bb192e9a5df3becfe40bb5646c22cbe3
arshia <arshiabohloli@gmail.com>
2025-02-07 20:22:45 +0330

    database and makefile setup

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,26 @@
 package main
 
-const webPort = "80"
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
 
 func main() {
-	
+	db := initDB()
+	db.Ping()
+}
+
+func initDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		log.Panic("can not connect to db")
+	}
+	return db
 }
