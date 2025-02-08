a065b4446342b3ebeb84a3079fb0d482fd818bd6
arshia <arshiabohloli@gmail.com>
2025-02-08 21:06:19 +0330

    config and logger setup

---
+++ cmd/web/config.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/alexedwards/scs/redisstore"
+	"github.com/alexedwards/scs/v2"
+	"github.com/gomodule/redigo/redis"
+)
+
+type Config struct {
+	Session    *scs.SessionManager
+	DB         *sql.DB
+	InfoLogger *log.Logger
+	ErroLogger *log.Logger
+	Wait       *sync.WaitGroup
+}
+
+func initDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		log.Panic("can not connect to db")
+	}
+	return db
+}
+
+func initSession() *scs.SessionManager {
+	session := scs.New()
+	//store all information from session in redis
+	session.Store = redisstore.New(initRedis())
+	session.Lifetime = time.Hour * 24
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = true
+	return session
+}
+
+func initRedis() *redis.Pool {
+	redis := redis.Pool{
+		MaxIdle: 10,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", os.Getenv("REDIS"))
+		},
+	}
+	return &redis
+}
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"log"
-	"net/http"
 	"os"
-	"time"
+	"sync"
 
-	"github.com/alexedwards/scs/redisstore"
-	"github.com/alexedwards/scs/v2"
-	"github.com/gomodule/redigo/redis"
 	_ "github.com/jackc/pgconn"
 	_ "github.com/jackc/pgx/v4"
 	_ "github.com/jackc/pgx/v4/stdlib"
@@ -17,37 +12,18 @@ import (
 
 func main() {
 	db := initDB()
-	db.Ping()
-
 	session := initSession()
-}
+	var wg sync.WaitGroup
 
-func initDB() *sql.DB {
-	dsn := os.Getenv("DSN")
-	db, err := sql.Open("pgx", dsn)
-	if err != nil {
-		log.Panic("can not connect to db")
+	infoLogger := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLogger := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := Config{
+		Session:    session,
+		DB:         db,
+		Wait:       &wg,
+		InfoLogger: infoLogger,
+		ErroLogger: errorLogger,
 	}
-	return db
-}
-
-func initSession() *scs.SessionManager {
-	session := scs.New()
-	//store all information from session in redis
-	session.Store = redisstore.New(initRedis())
-	session.Lifetime = time.Hour * 24
-	session.Cookie.Persist = true
-	session.Cookie.SameSite = http.SameSiteLaxMode
-	session.Cookie.Secure = true
-	return session
-}
-
-func initRedis() *redis.Pool {
-	redis := redis.Pool{
-		MaxIdle: 10,
-		Dial: func() (redis.Conn, error) {
-			return redis.Dial("tcp", os.Getenv("REDIS"))
-		},
-	}
-	return &redis
+	
 }
